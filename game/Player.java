17e58e59ef57563b01a4546e1f84bc3ac9b9394c
jvia <jvia@itu.dk>
2024-10-22 10:57:42 +0200

    Implement Player

---
+++ game/Player.java
@@ -0,0 +1,30 @@
+public class Player {
+    private int id;
+    private Account account;
+
+    public Player(int initialBalance, int ID) {
+        this.account = new Account(initialBalance);
+        this.id = ID;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public int getBalance(){
+        return account.getBalance();
+    }
+
+    public boolean withdraw(int amount) {
+        return account.withdraw(amount);
+    }
+
+    public boolean deposit(int amount) {
+        return account.deposit(amount);
+    }
+
+    public String toString() {
+        return "Player-ID: " + id + "\n" +
+                "Player account information: " + account;
+    }
+}
