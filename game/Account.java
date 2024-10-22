25303e0bc0dfd599e0420023af8ce86237a9e52d
Sally <88275731+SallyWinter@users.noreply.github.com>
2024-10-22 10:37:48 +0200

    "Account" made with methods and attribute

--- game/Account.java
+++ game/Account.java
@@ -1,3 +1,34 @@
 public class Account {
+    private int balance;    
+
+    public Account(int balance){
+        this.balance = balance;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public boolean withdraw(int amount){
+        if(balance - amount < 0 || amount < 0){
+            return false;
+        } else {
+            balance -= amount;
+            return true;
+        }
+    }
+
+    public boolean deposit(int amount){
+        if(amount < 0){
+            return false;
+        } else {
+            balance += amount;
+            return true;
+        }
+    }
     
+    public String toString(){
+        return "Balance: " + balance;
+    }
+
 }
