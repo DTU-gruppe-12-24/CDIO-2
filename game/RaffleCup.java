7740e818ad8adb10047cf3f195d861d095d35946
jvia <jvia@itu.dk>
2024-10-22 11:25:48 +0200

    Implement RaffleCup

--- game/RaffleCup.java
+++ game/RaffleCup.java
@@ -1,3 +1,35 @@
+package game;
+
 public class RaffleCup {
-    
+    private final Die[] DICE;
+
+    public RaffleCup(int sidesOfDie) {
+        DICE = new Die[]{new Die(sidesOfDie), new Die(sidesOfDie)};
+    }
+
+    public void roll(){
+        for (Die die : DICE) {
+            die.roll();
+        }
+    }
+
+    public int getValue(int index) {
+        return DICE[index].getFaceValue();
+    }
+
+    public int getSum() {
+        int sum = 0;
+        for (Die die : DICE) {
+            sum += die.getFaceValue();
+        }
+        return sum;
+    }
+
+    public String toString() {
+        String output = "The dice of the raffle-cup:" + System.lineSeparator();
+        for (Die die : DICE) {
+            output += die + System.lineSeparator();
+        }
+        return output;
+    }
 }
