bd2a60f98fa58c901f87d17f6e621089e7dfc7b6
Sally <88275731+SallyWinter@users.noreply.github.com>
2024-10-22 10:11:13 +0200

    made Die
    with sides and toString

---
+++ game/Die.java
@@ -0,0 +1,40 @@
+package game;
+
+/**
+ * A Die.
+ */
+public class Die {
+    private int faceValue; // Initial face value of the die.
+    private int sides; //  The sides of the die.
+
+    /**
+     * Constructor of the Die class.
+     * <br>
+     * We use the roll() method to get a random face value to reflect the
+     * real-world randomization of first starting using a die in a
+     * game.
+     */
+    public Die(int sides) {
+        this.sides = sides;
+        roll();
+    }
+
+    /**
+     * Rolls the die, updating its face value based on a uniform
+     * distribution.
+     */
+    public void roll() {
+        faceValue = (int) (Math.random() * sides) + 1;
+    }
+
+    /**
+     * @return the face value of the die
+     */
+    public int getFaceValue() {
+        return faceValue;
+    }
+
+    public String toString(){
+        return "facevalue: " + faceValue + ", sides: " + sides;
+    }
+}
