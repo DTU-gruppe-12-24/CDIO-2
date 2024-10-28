b25cc40828ee37e77572bcdf125b3d48f719b09b
Sally <88275731+SallyWinter@users.noreply.github.com>
2024-10-28 20:37:41 +0100

    Documented Account

--- game/Account.java
+++ game/Account.java
@@ -1,15 +1,30 @@
 package game;
 
+// Account class, each account only has a balance
 public class Account {
     private int balance;    
 
+    /**
+     * Player's constructor.
+     * @param balance The initial balance of the player's account.
+     */
     public Account(int balance){
         this.balance = balance;
     }
 
+    /** Accessor for the player's account balance. 
+     * @return balance
+     */
     public int getBalance(){
         return balance;
     }
+
+
+    /** Withdraws amount from balance if balance minus the amount is greater then 0 and the amount less then 0
+     * 
+     * @param amount The amount to withdraw from the player's account.
+     * @return Boolean to indicate success.
+     */
 
     public boolean withdraw(int amount){
         if(balance - amount < 0 || amount < 0){
@@ -19,6 +34,12 @@ public class Account {
             return true;
         }
     }
+
+     /** Deposit amount to balance if amount is not less then 0 
+     *
+     * @param amount The amount to deposit.
+     * @return Boolean to indicate success.
+     */
 
     public boolean deposit(int amount){
         if(amount < 0){
