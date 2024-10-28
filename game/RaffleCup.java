ec43412830acfc95ec93c6de36d65dd3a9a3396e
jakobvial <76407053+JohnJumpoJump@users.noreply.github.com>
2024-10-28 13:36:17 +0100

    Document RaffleCup

--- game/RaffleCup.java
+++ game/RaffleCup.java
@@ -1,22 +1,41 @@
 package game;
 
+/**
+ * Responsible for interacting with the dice.
+ */
 public class RaffleCup {
     private final Die[] DICE;
 
+    /**
+     *
+     * @param sidesOfDie The number of sides of the die.
+     */
     public RaffleCup(int sidesOfDie) {
         DICE = new Die[]{new Die(sidesOfDie), new Die(sidesOfDie)};
     }
 
+    /**
+     * Roll the dice, updating their face value.
+     */
     public void roll(){
         for (Die die : DICE) {
             die.roll();
         }
     }
 
+    /**
+     *
+     * @param index The index of the die to get the face value of.
+     * @return The face value of the specified die.
+     */
     public int getValue(int index) {
         return DICE[index].getFaceValue();
     }
 
+    /**
+     *
+     * @return Get the sum total of the dice face values.
+     */
     public int getSum() {
         int sum = 0;
         for (Die die : DICE) {
